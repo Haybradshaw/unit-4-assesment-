5bab45ce4a59c7958825b443e99d9775f9ad23bd
hayden <haybradshaw@gmail.com>
2019-07-12 16:14:56 -0500

    circles and selectors working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import CircleSelector from './components/CircleSelector/CircleSelector';
+import Circles from './components/Circles/Circles';
 
 class App extends Component {
+
+  constructor(){
+    super()
+      this.state={
+       selected: '1'
+    }
+  }
+
+  changeName = (newSelection) =>{
+		this.setState({
+      selected: newSelection
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
         <main>
-          YOUR TWO COMPONENTS WILL GO HERE
+          <CircleSelector 
+            selected={this.state.selected}
+            onChange={this.changeName}
+          />
+          <Circles 
+            selected={this.state.selected}
+          />
         </main>
       </div>
     );
